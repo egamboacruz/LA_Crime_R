dabfc8170df247d0c4f9a29928e3fadb58337e7d
egamboacruz <egamboacruz0329@gmail.com>
2022-07-17 16:22:23 -0700

    New File

--- CrimeScript.R
+++ CrimeScript.R
@@ -23,11 +23,94 @@ data <- data %>%
 #format it to POSIXct
 data$datetime_occ <- ymd_hm(data$datetime_occ)
 
+# I want to change victim descent from abbreviation to the whole word.
+# Could also use dplyr::case_when
+data$vict_descent[data$vict_descent == "A"] <- "Asian"
+data$vict_descent[data$vict_descent == "B"] <- "Black"
+data$vict_descent[data$vict_descent == "C"] <- "Chineese"
+data$vict_descent[data$vict_descent == "D"] <- "Cambodian"
+data$vict_descent[data$vict_descent == "F"] <- "Filipino"
+data$vict_descent[data$vict_descent == "G"] <- "Guamaniam"
+data$vict_descent[data$vict_descent == "H"] <- "Hispanic/Latin/Mexican"
+data$vict_descent[data$vict_descent == "I"] <- "American Indian"
+data$vict_descent[data$vict_descent == "J"] <- "Japanese"
+data$vict_descent[data$vict_descent == "K"] <- "Korean"
+data$vict_descent[data$vict_descent == "L"] <- "Laotian"
+data$vict_descent[data$vict_descent == "O"] <- "Other"
+data$vict_descent[data$vict_descent == "P"] <- "Pacific Islander"
+data$vict_descent[data$vict_descent == "S"] <- "Samoan"
+data$vict_descent[data$vict_descent == "U"] <- "Hawaiian"
+data$vict_descent[data$vict_descent == "V"] <- "Vietnamese"
+data$vict_descent[data$vict_descent == "W"] <- "White"
+data$vict_descent[data$vict_descent == "X"] <- "Unkown"
+data$vict_descent[data$vict_descent == "Z"] <- "Asian Indian"
 
+min(data$vict_age) # -1 ??? what does that mean
+max(data$vict_age) # 120 years old.
+
+# Check if ages -1:1 years are victim ages or unknowns
+#count how many ages between -1 and 1
+data %>%
+  select(vict_age,vict_sex) %>% 
+  filter(between(vict_age,-1,1), vict_sex != "X") %>% 
+  group_by(vict_sex,vict_age) %>% 
+  count(vict_sex)
+
+# I strongly believe Age zero stands for someone calling 911 
+# their age was not given or recorded. 
+print(data %>% 
+        select(vict_age,vict_sex,crm_cd_desc) %>% 
+        filter(vict_age == 0,vict_sex != "X") %>% 
+        tail(),n=100)
+
+# checking what age -1 represents
+print(data %>% 
+        select(vict_age,vict_sex,crm_cd_desc) %>% 
+        filter(vict_age == -1,vict_sex != "X") %>% 
+        tail(),n=100)
+# out of over 500,000 observations it seems that -1 is a mistake
+
+#Create an age group and a development stage. helps with the analysis.
+# Development stage.
+data <- data %>%
+  mutate(
+    dev_stage = dplyr::case_when(
+      vict_age <= 1 ~ "Unkown",
+      vict_age > 1 & vict_age <=4 ~ "Toddler",
+      vict_age > 4 & vict_age <=12 ~ "Child",
+      vict_age > 12 & vict_age <=19 ~ "Teen",
+      vict_age > 19 & vict_age <=39 ~ "Adult",
+      vict_age > 39 & vict_age <=59 ~ "Middle Age",
+      vict_age > 59  ~ "Senior"
+    )
+  )
+# Age Group
+data <- data %>%
+  mutate(
+    age_group = dplyr::case_when(
+      vict_age <= 1 ~ "Unkown",
+      vict_age > 1 & vict_age <=4 ~ "2-4",
+      vict_age > 4 & vict_age <=12 ~ "5-12",
+      vict_age > 12 & vict_age <=19 ~ "13-19",
+      vict_age > 19 & vict_age <=39 ~ "20-39",
+      vict_age > 39 & vict_age <=59 ~ "40-59",
+      vict_age > 59  ~ "60+"
+    )
+  )
+
+#dr_no is a case number should be a string not numerical
+data$dr_no <- as.character(data$dr_no)
 
 # Rank Crime In LA (by year)
+data %>% 
+  select(date_occ) %>% 
+  group_by(Year=year(date_occ)) %>% 
+  count(date_occ) %>% 
+  ggplot(aes(x=date_occ,y=n)) +
+  geom_point()
+  
 
-# Comparison of crime from 2020-to present
+ # Comparison of crime from 2020-to present
 
 # Crime Female vs Male
 
